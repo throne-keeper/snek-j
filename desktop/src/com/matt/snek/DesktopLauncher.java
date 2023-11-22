b3d8c1039a85aa00fedbe9c23dc78f923a1336f9
Matt Hardy <matthardy@Midnight-Rain.local>
2023-11-21 19:44:40 -0700

    Initial commit

---
+++ desktop/src/com/matt/snek/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.matt.snek;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.matt.snek.GameEngine;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("snek-j");
+		new Lwjgl3Application(new GameEngine(), config);
+	}
+}
